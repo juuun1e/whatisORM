112e3dbf1364d9ca9c124963c401c1e75746d78b
juuun1e <wldn5959@naver.com>
2024-02-02 16:39:03 +0900

    Security Config설정(특정경로 사용자제한, login, CSRF비활성화,logout 설정

--- club/src/main/java/com/zerock/club/config/SecurityConfig.java
+++ club/src/main/java/com/zerock/club/config/SecurityConfig.java
@@ -3,12 +3,17 @@ package com.zerock.club.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 //모든 시큐리티 관련 설정이 추가되는 부분
 @Configuration
@@ -33,5 +38,26 @@ public class SecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
+
+    //인가가 필요한 리소스 설정 방법 : 1)Security Config 설정을 통해 패턴 지정, 2)어노테이션 이용해 접근 제한
+    //filterChain()을 작성하여 세밀한 인증/인가에 대한 처리 추가 - HttpSecurity API를 이용하여 특정한 경로 설정 가능
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        log.info("------------filterChain-----------");
+        http
+                .authorizeRequests(authorizeRequests ->   authorizeRequests
+                                        .requestMatchers("/sample/all").permitAll() //모든 사용자에게 허가
+                                        .requestMatchers("/sample/member").hasRole("USER")
+                )
+                .formLogin(withDefaults()) // 인증, 인가 시 로그인 화면
+                .csrf(csrf -> csrf.disable()) //CSRF(Cross Site Request Forgery) - 비활성화
+                .logout((logout) -> logout
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+
+
 
 }
