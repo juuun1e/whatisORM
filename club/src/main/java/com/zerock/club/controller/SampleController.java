f7e1529f54e9444328afcc96bab74a08a85d8f01
juuun1e <wldn5959@naver.com>
2024-02-04 23:51:14 +0900

    Thymeleaf와 controller에서 사용자 정보 출력하기

--- club/src/main/java/com/zerock/club/controller/SampleController.java
+++ club/src/main/java/com/zerock/club/controller/SampleController.java
@@ -1,6 +1,8 @@
 package com.zerock.club.controller;
 
+import com.zerock.club.security.dto.ClubAuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +20,12 @@ public class SampleController {
     }
 
     // 로그인한 사용자만 접근 가능한
-    @GetMapping("/member")
-    public void exMember(){
+    @GetMapping("/member") //@AuthenticationPrincipal어노테이션을 사용하여 로그인된 사용자 정보를 확인
+    public void exMember(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember){
 
         log.info(".....exMember.....");
+        log.info("===================");
+        log.info(clubAuthMember);
 
     }
 
