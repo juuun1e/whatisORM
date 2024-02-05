03108a59f6086a15884695d67fb71f7985958a7e
juuun1e <wldn5959@naver.com>
2024-02-06 00:27:10 +0900

    rememberMe와 @PreAuthorize +파라미터값 사용하기(@PreAuthorize의 사용)

--- club/src/main/java/com/zerock/club/config/SecurityConfig.java
+++ club/src/main/java/com/zerock/club/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.zerock.club.security.handler.ClubLoginSuccessHandler;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,8 +15,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 //모든 시큐리티 관련 설정이 추가되는 부분
-@Configuration
-@EnableWebSecurity
+@Configuration //자바 기반의 설정 클래스를 정의할 때 사용, 스프링 컨테이너는 이 클래스를 빈으로 등록함.
+@EnableWebSecurity //웹 보안 활성화 됨, 주로 전역적인 웹 보안 구성을 활성화하는데 사용
+@EnableMethodSecurity //메소드 수준의 보안을 활성화하는 데 사용(@PreAuthorize, @PostAuthorize, @PreFilter, @PostFilter)
 @Log4j2
 public class SecurityConfig {
 
@@ -53,8 +56,8 @@ public class SecurityConfig {
                 .logout((logout) -> logout
                         .permitAll())
             .oauth2Login(oauth2 -> oauth2
-                .successHandler(clubLoginSuccessHandler()) // OAuth2 로그인 성공 시 처리
-            );
+                .successHandler(clubLoginSuccessHandler())) // OAuth2 로그인 성공 시 처리
+            .rememberMe(Customizer.withDefaults());
         return http.build();
     }
 
--- club/src/main/java/com/zerock/club/controller/SampleController.java
+++ club/src/main/java/com/zerock/club/controller/SampleController.java
@@ -2,6 +2,7 @@ package com.zerock.club.controller;
 
 import com.zerock.club.security.dto.ClubAuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/sample")
 public class SampleController {
     // 로그인 하지 않은 사용자도 접근할 수 있는
+    @PreAuthorize("permitAll()")
     @GetMapping("/all")
     public void exAll(){
 
@@ -30,10 +32,25 @@ public class SampleController {
     }
 
     // 관리자 권한이 있는 사용자만 접근 가능한
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public void exAdmin(){
 
-        log.info(".....exAdmin.....");
+        log.info(".....exAdmin......");
 
+    }
+
+    //파라미터 값 이용하기
+    //사용자 중에서 특별히 정해진 사용자만이 해당 메서드를 실행하고 싶은 경우
+    //PreAuthorize의 value표현식은 #과 같은 특별한 기호나 authenticaion 같은 내장변수 이용 가능
+    //user95 사용자만 접근 가능하게 만들고 싶은 경우의 설정
+    @PreAuthorize("#clubAuthMember != null && #clubAuthMember.username eq\"user95@zerock.org\"")
+    @GetMapping("/exOnly")
+    public String exMemberOnly(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember){
+
+        log.info(".....exMemberOnly......");
+        log.info(clubAuthMember);
+
+        return "/sample/admin";
     }
 }
